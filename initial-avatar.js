e3d4295d8ae7f0765ac1859653bbae755d5c928b
Shaun Blake <sfblake@hawaii.edu>
2018-08-08 17:56:26 -1000

    Make colors match uppercase initials as well.

--- initial-avatar.js
+++ initial-avatar.js
@@ -42,32 +42,32 @@ class InitialAvatar extends LitElement {
         }
         /* The .default needs to appear first so it gets overriden by the other rules */
         .default {background-color: var(--bg-color, #E57373);}
-        .a {background-color: var(--a-bg-color, #E57373);}
-        .b {background-color: var(--b-bg-color, #7986CB);}
-        .c {background-color: var(--c-bg-color, #64B5F6);}
-        .d {background-color: var(--d-bg-color, #4DB6AC);}
-        .e {background-color: var(--e-bg-color, #388E3C);}
-        .f {background-color: var(--f-bg-color, #FFB74D);}
-        .g {background-color: var(--g-bg-color, #FF8A65);}
-        .h {background-color: var(--h-bg-color, #A1887F);}
-        .i {background-color: var(--i-bg-color, #E0E0E0);}
-        .j {background-color: var(--j-bg-color, #90A4AE);}
-        .k {background-color: var(--k-bg-color, #E57373);}
-        .l {background-color: var(--l-bg-color, #7986CB);}
-        .m {background-color: var(--m-bg-color, #64B5F6);}
-        .n {background-color: var(--n-bg-color, #4DB6AC);}
-        .o {background-color: var(--o-bg-color, #388E3C);}
-        .p {background-color: var(--p-bg-color, #FFB74D);}
-        .q {background-color: var(--q-bg-color, #FF8A65);}
-        .r {background-color: var(--r-bg-color, #A1887F);}
-        .s {background-color: var(--s-bg-color, #E0E0E0);}
-        .t {background-color: var(--t-bg-color, #90A4AE);}
-        .u {background-color: var(--u-bg-color, #E57373);}
-        .v {background-color: var(--v-bg-color, #7986CB);}
-        .w {background-color: var(--w-bg-color, #64B5F6);}
-        .x {background-color: var(--x-bg-color, #4DB6AC);}
-        .y {background-color: var(--y-bg-color, #388E3C);}
-        .z {background-color: var(--z-bg-color, #FFB74D);}
+        .a .A {background-color: var(--a-bg-color, #E57373);}
+        .b .B {background-color: var(--b-bg-color, #7986CB);}
+        .c .C {background-color: var(--c-bg-color, #64B5F6);}
+        .d .D {background-color: var(--d-bg-color, #4DB6AC);}
+        .e .E {background-color: var(--e-bg-color, #388E3C);}
+        .f .F {background-color: var(--f-bg-color, #FFB74D);}
+        .g .G {background-color: var(--g-bg-color, #FF8A65);}
+        .h .H {background-color: var(--h-bg-color, #A1887F);}
+        .i .I {background-color: var(--i-bg-color, #E0E0E0);}
+        .j .J {background-color: var(--j-bg-color, #90A4AE);}
+        .k .K {background-color: var(--k-bg-color, #E57373);}
+        .l .L {background-color: var(--l-bg-color, #7986CB);}
+        .m .M {background-color: var(--m-bg-color, #64B5F6);}
+        .n .N {background-color: var(--n-bg-color, #4DB6AC);}
+        .o .O {background-color: var(--o-bg-color, #388E3C);}
+        .p .P {background-color: var(--p-bg-color, #FFB74D);}
+        .q .Q {background-color: var(--q-bg-color, #FF8A65);}
+        .r .R {background-color: var(--r-bg-color, #A1887F);}
+        .s .S {background-color: var(--s-bg-color, #E0E0E0);}
+        .t .T {background-color: var(--t-bg-color, #90A4AE);}
+        .u .U {background-color: var(--u-bg-color, #E57373);}
+        .v .V {background-color: var(--v-bg-color, #7986CB);}
+        .w .W {background-color: var(--w-bg-color, #64B5F6);}
+        .x .X {background-color: var(--x-bg-color, #4DB6AC);}
+        .y .Y {background-color: var(--y-bg-color, #388E3C);}
+        .z .Z {background-color: var(--z-bg-color, #FFB74D);}
       </style>
       <div id="initial" class$="default ${initial} ${shapeClass}">${initial}</div>
     `;
